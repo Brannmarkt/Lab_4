d83d1adec9b5a2496f8ccecc7fe141eabee5c77c
Brannmarkt <117072318+Brannmarkt@users.noreply.github.com>
2023-03-12 18:12:40 +0200

    Add files via upload

---
+++ Lab_4/Lab_4/Entities/Discipline.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4.Entities
+{
+    public class Discipline
+    {
+        public string Title { get; set; }
+        public int Hours { get; set; }
+
+        public Discipline()
+        {
+            Title = string.Empty;
+            Hours = 0;
+        }
+
+        public Discipline(string title, int hours)
+        {
+            Title = title; 
+            Hours = hours;
+        }
+    }
+}
---
+++ Lab_4/Lab_4/Entities/Teacher.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4.Entities
+{
+    public class Teacher
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+        public List<Discipline> Disciplines { get; set; }
+
+        public Teacher() 
+        {
+            Name = string.Empty;
+            Salary = 0;
+            Disciplines = new List<Discipline>();
+        }
+
+        public Teacher(string name, double salary)
+        {
+            Name = name;
+            Salary = salary;
+            Disciplines = new List<Discipline>();
+        }
+
+        public IEnumerable<Discipline> GetDisciplines()
+        {
+            foreach (var item in Disciplines)
+            {
+                yield return item;
+            }
+        }
+
+        public void Add_Discipline(Discipline new_discipline)
+        {
+            Disciplines.Add(new_discipline);
+        }
+
+    }
+}
